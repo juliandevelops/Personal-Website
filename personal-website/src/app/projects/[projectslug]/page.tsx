8789fee12b14bb37be64d7442c9853ef250a196a
Julian Schumacher <professional@julianschumacher.dev>
2026-01-06 18:47:32 +0100

    Update links

--- personal-website/src/app/projects/[projectslug]/page.tsx
+++ personal-website/src/app/projects/[projectslug]/page.tsx
@@ -2,5 +2,5 @@ import { permanentRedirect } from "next/navigation";
 
 export default async function ProjectSlugRedirect({ params }: { params: Promise<{ projectslug: string }> }) {
     const { projectslug } = await params;
-    permanentRedirect(`https://juliandevelops.com/projects/${projectslug}`);
+    permanentRedirect(`https://juliandevelops.com/work/projects/${projectslug}`);
 }

5db3f59b97b42cf948403a3fb472b16fa60ea74c
Julian Schumacher <professional@julianschumacher.dev>
2026-01-06 18:45:46 +0100

    Add redirect to new page

---
+++ personal-website/src/app/projects/[projectslug]/page.tsx
@@ -0,0 +1,6 @@
+import { permanentRedirect } from "next/navigation";
+
+export default async function ProjectSlugRedirect({ params }: { params: Promise<{ projectslug: string }> }) {
+    const { projectslug } = await params;
+    permanentRedirect(`https://juliandevelops.com/projects/${projectslug}`);
+}
---
+++ personal-website/src/app/projects/page.tsx
@@ -0,0 +1,5 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function ProjectRedirect() {
+    permanentRedirect("https://juliandevelops.com/projects");
+}
